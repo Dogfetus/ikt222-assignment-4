a7b3eac1bf75835659c3abd19c80d45a35380c64
Erlend Tregde <erlendt@uia.no>
2024-11-18 19:50:56 +0100

    added hashed password

--- src/main/java/IKT222/patients/AppServlet.java
+++ src/main/java/IKT222/patients/AppServlet.java
@@ -23,11 +23,14 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateExceptionHandler;
 
+import org.mindrot.jbcrypt.BCrypt; // adding bcrypt to hash the password
+
 @SuppressWarnings("serial")
 public class AppServlet extends HttpServlet {
 
   private static final String CONNECTION_URL = "jdbc:sqlite:db.sqlite3";
   private static final String AUTH_QUERY = "select * from user where username=? and password=?";
+  private static final String AUTH_QUERY2 = "SELECT password FROM user WHERE username = ?"; // for auth function
   private static final String SEARCH_QUERY = "select * from patient where surname like ?";
 
   private final Configuration fm = new Configuration(Configuration.VERSION_2_3_28);
@@ -37,6 +40,12 @@ public class AppServlet extends HttpServlet {
   public void init() throws ServletException {
     configureTemplateEngine();
     connectToDatabase();
+    // adding the adduser function to check if the hashing works
+    try {
+      addAUser("SanderW", "Sander", "password1.");
+    } catch (SQLException e) {
+        throw new ServletException("Failed to add user: " + e.getMessage(), e);
+    }
   }
 
   private void configureTemplateEngine() throws ServletException {
@@ -102,16 +111,48 @@ public class AppServlet extends HttpServlet {
     }
   }
 
-  private boolean authenticated(String username, String password) throws SQLException {
-    try (PreparedStatement stmt = database.prepareStatement(AUTH_QUERY)) {
-      stmt.setString(1, username);
-      stmt.setString(2, password);
-      ResultSet results = stmt.executeQuery();
-      return results.next();
-    }
-  }
+ // private boolean authenticated(String username, String password) throws SQLException {
+ //   try (PreparedStatement stmt = database.prepareStatement(AUTH_QUERY)) {
+ //     stmt.setString(1, username);
+ //     stmt.setString(2, password);
+ //     ResultSet results = stmt.executeQuery();
+ //     return results.next();
+ //   }
+ // }
 
-  private List<Record> searchResults(String surname) throws SQLException {
+// the updated authenticated function
+private boolean authenticated(String username, String password) throws SQLException {
+        // using PreparedStatement to stop sql injection
+        try (PreparedStatement stmt = database.prepareStatement(AUTH_QUERY2)) {
+            stmt.setString(1, username); 
+            ResultSet results = stmt.executeQuery();
+            if (results.next()) {
+                String storedHashPassword = results.getString("password");
+                return BCrypt.checkpw(password, storedHashPassword); 
+            }
+        }
+        return false;
+    }
+
+// function to hash password using bcrypt
+public String hashPassword(String password) {
+    return BCrypt.hashpw(password, BCrypt.gensalt());
+}    
+
+// added just for testing purposes, we use this fuction in the init() function to test if the hashing works
+public void addAUser(String name, String username, String plainPassword) throws SQLException {
+    String hashedPassword = hashPassword(plainPassword); //hashing the password
+    String Query = "INSERT INTO user (name, username, password) VALUES (?, ?, ?)"; //making the insert query
+    try (PreparedStatement stmt = database.prepareStatement(Query)) {
+        // adding name, username and hased password to the insert query
+        stmt.setString(1, name);
+        stmt.setString(2, username);
+        stmt.setString(3, hashedPassword);
+        stmt.executeUpdate();
+    }
+}
+
+private List<Record> searchResults(String surname) throws SQLException {
     List<Record> records = new ArrayList<>();
     try (PreparedStatement stmt = database.prepareStatement(SEARCH_QUERY)){
       stmt.setString(1, surname);
